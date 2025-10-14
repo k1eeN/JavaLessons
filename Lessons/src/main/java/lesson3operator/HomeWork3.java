ac1ab0407a2dbaa1ce7be1ee1d4885970a0042cc
Mark Klinitskii <mark23klin@mail.ru>
2025-10-15 01:43:25 +0300

    Lesson 3 - Operator

---
+++ Lessons/src/main/java/lesson3operator/HomeWork3.java
@@ -0,0 +1,14 @@
+package lesson3operator;
+
+public class HomeWork3 {
+    public static void main(String[] args) {
+        int money = 10;
+        if (money > 500) {
+            System.out.println("Покупаю пиццу");
+        } else if (money > 300) {
+            System.out.println("Покупаю шаурму");
+        } else if (money < 100) {
+            System.out.println("Покупаю дошик");
+        }
+    }
+}
