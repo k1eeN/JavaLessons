41928f253752ac8fbb902a9f020f981ed33fff2e
Mark Klinitskii <mark23klin@mail.ru>
2025-10-14 17:41:45 +0300

    Lesson2

---
+++ Lessons/src/main/java/lesson1/HomeWork1.java
@@ -0,0 +1,9 @@
+package lesson1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        System.out.println("Olesya");
+        System.out.println("Ilya");
+        System.out.println("Elena");
+    }
+}
