bc9eecbe2b633cbdd952b90411a3eafdcb3eb0ca
Mark Klinitskii <mark23klin@mail.ru>
2025-10-17 00:11:11 +0300

    Lesson 7 - Array

---
+++ Lessons/src/main/java/lesson7array/HomeWork7.java
@@ -0,0 +1,17 @@
+package lesson7array;
+
+public class HomeWork7 {
+    public static void main(String[] args) {
+        int[] first = new int[900];
+        for (int i = 0; i < first.length; i++) {
+            first[i] = i + 100;
+        }
+        int[] second = new int[first.length];
+        for (int i = 0, j = first.length - 1; i < first.length; i++, j--) {
+            second[j] = first[i];
+        }
+        for (int i : second) {
+            System.out.println(i);
+        }
+    }
+}
