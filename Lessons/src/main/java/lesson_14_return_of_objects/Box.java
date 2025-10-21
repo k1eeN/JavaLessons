4bfbe27b83b798272b22a92a428729829fbcb5d7
Mark Klinitskii <mark23klin@mail.ru>
2025-10-21 19:03:35 +0300

    Lesson 14 - Return Of Objects

---
+++ Lessons/src/main/java/lesson_14_return_of_objects/Box.java
@@ -0,0 +1,41 @@
+package lesson_14_return_of_objects;
+
+public class Box {
+    double width;
+    double height;
+    double length;
+
+    Box(double width, double height, double length) {
+        this.width = width;
+        this.height = height;
+        this.length = length;
+    }
+
+    Box(double size) {
+        width = size;
+        height = size;
+        length = size;
+    }
+
+    Box(Box box1, Box box2) {
+        this.width = box1.width + box2.width;
+        this.length = box1.length + box2.length;
+        this.height = box1.height + box2.height;
+    }
+
+    Box sumBox(Box box) {
+        return new Box(this.width + box.width, this.height + box.height, this.length + box.length);
+    }
+
+    double volume() {
+        return width * height * length;
+    }
+
+    void showVolume() {
+        System.out.println(volume());
+    }
+}
+
+
+
+
