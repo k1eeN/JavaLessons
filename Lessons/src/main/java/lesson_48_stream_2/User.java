644de444f349ba65481cbc6cca7acb7f2b796ef9
Mark Klinitskii <mark23klin@mail.ru>
2025-11-06 17:27:06 +0300

    Lesson 48 - Stream 2

---
+++ Lessons/src/main/java/lesson_48_stream_2/User.java
@@ -0,0 +1,27 @@
+package lesson_48_stream_2;
+
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
